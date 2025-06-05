fc7efbb322a315bdc292dcf0b5136ce36281a610
SuThetHnin03 <suthethnin24603@gmail.com>
2025-06-05 21:04:00 +0630

    replace browserRouter with hashrouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,44 @@
 import HomePage from './pages/HomePage'
 import Header from './components/Header'
 import SideNav from './components/SideNav'
-import { Outlet, useLocation } from 'react-router'
-import "./styles/index.css"
+import { Routes, Route, Outlet, useLocation } from 'react-router-dom'
 import { useState } from 'react'
 
-const App = () => {
-    const [isOpen, setIsOpen] = useState(true)
-    const location = useLocation()
+import SkillsPage from './pages/SkillsPage'
+import ProjectsPage from './pages/ProjectsPage'
+import ProjectPage from './pages/ProjectPage'
+import ContactPage from './pages/ContactPage'
+import ResourcesPage from './pages/ResourcesPage'
+import NotFound from './pages/NotFound'
+
+const Layout = () => {
+  const [isOpen, setIsOpen] = useState(true)
+  const location = useLocation()
+
   return (
     <div>
-      <Header/>
-      <SideNav isOpen={isOpen} setIsOpen={setIsOpen}/>
-      <div className={`pages ${isOpen ? "" : "moved"}`}>
-       {
-         location.pathname == '/' && <HomePage/>
-       }
-      <Outlet/>
+      <Header />
+      <SideNav isOpen={isOpen} setIsOpen={setIsOpen} />
+      <div className={`pages ${isOpen ? '' : 'moved'}`}>
+        {location.pathname === '/' && <HomePage />}
+        <Outlet />
       </div>
     </div>
+  )
+}
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route path="skills" element={<SkillsPage />} />
+        <Route path="projects" element={<ProjectsPage />} />
+        <Route path="projects/:projectId" element={<ProjectPage />} />
+        <Route path="contact" element={<ContactPage />} />
+        <Route path="resources" element={<ResourcesPage />} />
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
   )
 }
 
